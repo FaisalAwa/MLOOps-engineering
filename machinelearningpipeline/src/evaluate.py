8076d25688c301efc8e2e5bd9a605429ebfca20d
Faisal Mehmood Awan <96324075+FaisalAwa@users.noreply.github.com>
2025-10-14 15:03:06 +0500

    Update evaluate.py

--- machinelearningpipeline/src/evaluate.py
+++ machinelearningpipeline/src/evaluate.py
@@ -7,9 +7,9 @@ import mlflow
 from urllib.parse import urlparse
 
 
-os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/krishnaik06/machinelearningpipeline.mlflow"
-os.environ['MLFLOW_TRACKING_USERNAME']="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ecee2adb4e36a250ae3251f"
+os.environ['MLFLOW_TRACKING_URI']=""
+os.environ['MLFLOW_TRACKING_USERNAME']=""
+os.environ["MLFLOW_TRACKING_PASSWORD"]=""
 
 
 # Load parameters from params.yaml
@@ -20,7 +20,7 @@ def evaluate(data_path,model_path):
     X = data.drop(columns=["Outcome"])
     y = data["Outcome"]
 
-    mlflow.set_tracking_uri("https://dagshub.com/krishnaik06/machinelearningpipeline.mlflow")
+    mlflow.set_tracking_uri("")
 
     ## load the model from the disk
     model=pickle.load(open(model_path,'rb'))
